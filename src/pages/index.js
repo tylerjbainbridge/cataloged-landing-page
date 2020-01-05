5fc84289f77e1144cb1d6920e9c2be1741f69dc6
Tyler Bainbridge <tyler@tylerbainbridge.com>
2020-01-05 15:44:44 -0500

    removed readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,100 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import styled, { createGlobalStyle } from "styled-components"
+
+import "../typography.css"
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: white;
+  }
+`
+
+export const HorizantalCenter = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const VerticalCenter = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`
+
+export const Main = styled.div`
+  position: relative;
+  top: -100px;
+  width: 700px;
+`
+
+const Title = styled.div`
+  font-size: 120px;
+  font-family: "Cooper BT";
+  color: black;
+  padding: 30px;
+  border: 10px solid #000;
+
+  @media (max-width: 768px) {
+    font-size: 120px;
+    flex-direction: column;
+  }
+`
+
+const InputContainer = styled.div`
+  margin-top: 50px;
+`
+
+const Button = styled.button`
+  color: white;
+  background-color: #ffcc5b;
+  font-family: "Cooper BT";
+  font-size: 25px;
+  padding: 10px;
+  margin-left: 20px;
+  cursor: pointer;
+
+  border: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+`
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 5px dotted #000;
+  padding: 10px;
+  font-size: 20px;
+  width: 400px;
+  outline: none;
+  font-family: "Courier New";
+  letter-spacing: 3px;
+  font-weight: 600;
+`
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <>
+    <GlobalStyles />
+    <Container>
+      <Main>
+        <HorizantalCenter>
+          <Title>Hullabaloo</Title>
+        </HorizantalCenter>
+        <HorizantalCenter>
+          <InputContainer>
+            <Input placeholder="Enter email" />
+            <Button>Join waitlist</Button>
+          </InputContainer>
+        </HorizantalCenter>
+      </Main>
+    </Container>
+  </>
 )
 
 export default IndexPage
